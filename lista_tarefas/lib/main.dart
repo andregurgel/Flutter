96be94fa40c9cc8bb690d9e1c358b8f92cf14c8b
Andre Gurgel <andreggurgelufrn@gmail.com>
2020-06-07 15:18:59 -0300

    Iniciando lista para mostrar os itens.

--- lista_tarefas/lib/main.dart
+++ lista_tarefas/lib/main.dart
@@ -18,7 +18,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  List _toDoList = [];
+  List _toDoList = ["Andre", "Daniel"];
 
   @override
   Widget build(BuildContext context) {
@@ -50,7 +50,22 @@ class _HomeState extends State<Home> {
                 )
               ],
             ),
-          )
+          ),
+          Expanded(
+            child: ListView.builder(
+                padding: EdgeInsets.only(top: 10),
+                itemCount: _toDoList.length,
+                itemBuilder: (context, index){
+                  return CheckboxListTile(
+                    title: Text(_toDoList[index]["title"]),
+                    value: _toDoList[index]["ok"],
+                    secondary: CircleAvatar(
+                      child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
+                    ),
+                  );
+                },
+            ),
+          ),
         ],
       ),
     );
