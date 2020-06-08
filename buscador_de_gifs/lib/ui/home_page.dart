c2dc334d3d874aa47fea3b614d1c89920aa3f778
Andre Gurgel <andreggurgelufrn@gmail.com>
2020-06-08 17:58:05 -0300

    COmpartilhamento de gifs funcionando.

--- buscador_de_gifs/lib/ui/home_page.dart
+++ buscador_de_gifs/lib/ui/home_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -121,6 +122,9 @@ class _HomePageState extends State<HomePage> {
                 Navigator.push(context,
                 MaterialPageRoute(builder: (context) => GifPage(snapshot.data["data"][index]))
                 );
+              },
+              onLongPress: (){
+                Share.share(snapshot.data["data"][index]["images"]["fixed_height"]["url"]);
               },
             );
           }
