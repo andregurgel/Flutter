85b690884c07e4fb9c1f49eec742e122511b39fd
Andre Gurgel <andreggurgelufrn@gmail.com>
2020-06-08 16:11:03 -0300

    Requisitando dados.

--- buscador_de_gifs/lib/main.dart
+++ buscador_de_gifs/lib/main.dart
@@ -1,20 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:buscador_de_gifs/ui/home_page.dart';
+
+const request_trending = "https://api.giphy.com/v1/gifs/trending?api_key=4Y0Qf5z3vTBnI08KKMdmUhzFljtSzfIZ&limit=20&rating=G";
+const request_search = "https://api.giphy.com/v1/gifs/search?api_key=4Y0Qf5z3vTBnI08KKMdmUhzFljtSzfIZ&q=&limit=25&offset=0&rating=G&lang=en";
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    home: HomePage(),
   ));
 }
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
