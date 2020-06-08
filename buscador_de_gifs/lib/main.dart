5b0cd769147a360eaf1f0668f81ae9a99bf32fa3
Andre Gurgel <andreggurgelufrn@gmail.com>
2020-06-08 16:38:31 -0300

    Iniciando montagem do layout.

--- buscador_de_gifs/lib/main.dart
+++ buscador_de_gifs/lib/main.dart
@@ -2,12 +2,25 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:buscador_de_gifs/ui/home_page.dart';
 
-const request_trending = "https://api.giphy.com/v1/gifs/trending?api_key=4Y0Qf5z3vTBnI08KKMdmUhzFljtSzfIZ&limit=20&rating=G";
-const request_search = "https://api.giphy.com/v1/gifs/search?api_key=4Y0Qf5z3vTBnI08KKMdmUhzFljtSzfIZ&q=&limit=25&offset=0&rating=G&lang=en";
-
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    theme: ThemeData(
+      hintColor: Colors.white,
+      inputDecorationTheme: InputDecorationTheme(
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white
+          )
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          )
+        ),
+        hintStyle: TextStyle(color: Colors.white)
+      )
+    ),
   ));
 }
 
